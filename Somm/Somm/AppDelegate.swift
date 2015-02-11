b388f426814d812a55e124ca020828c2df70b4fa
Connor Knabe <ConnorKnabe@gmail.com>
2015-02-11 10:35:14 -0600

    Starting a fresh project using single view

--- Somm/Somm/AppDelegate.swift
+++ Somm/Somm/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  Somm
 //
-//  Created by Connor Knabe on 2/5/15.
+//  Created by Connor Knabe on 2/11/15.
 //  Copyright (c) 2015 SommApp. All rights reserved.
 //
 
--- Somm/Somm/ViewController.swift
+++ Somm/Somm/ViewController.swift
@@ -1,14 +1,14 @@
 //
-//  SecondViewController.swift
+//  ViewController.swift
 //  Somm
 //
-//  Created by Connor Knabe on 2/5/15.
+//  Created by Connor Knabe on 2/11/15.
 //  Copyright (c) 2015 SommApp. All rights reserved.
 //
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
