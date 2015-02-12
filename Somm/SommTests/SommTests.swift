17402bcc26a5161907a040ca6d6faf8ed80f19ac
Connor Knabe <ConnorKnabe@gmail.com>
2015-02-11 19:56:21 -0600

    Breaks constraints but looks good

--- Somm/SommTests/SommTests.swift
+++ Somm/SommTests/SommTests.swift
@@ -3,7 +3,7 @@
 //  SommTests
 //
 //  Created by Connor Knabe on 2/11/15.
-//  Copyright (c) 2015 SommApp. All rights reserved.
+//  Copyright (c) 2015 Somm. All rights reserved.
 //
 
 import UIKit
