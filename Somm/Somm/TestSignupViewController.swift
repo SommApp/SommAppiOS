83307ddb10b2f9c2206fa8577ebf174daf8473cf
Connor Knabe <ConnorKnabe@gmail.com>
2015-02-25 09:05:50 -0600

    Wrote a failing test

--- Somm/Somm/TestSignupViewController.swift
+++ Somm/Somm/TestSignupViewController.swift
@@ -10,30 +10,15 @@ import UIKit
 import XCTest
 
 class TestSignupViewController: XCTestCase {
-    let testObj: UIViewController = SignupViewController()
+    var testObj :SignupViewController?
 
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        testObj = SignupViewController()
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    func testIsUsernamePassBlank_Returns_True_Given_Blank_username_password() {
+        var result = testObj?.isUsernamePassBlank("", password: "")
+        XCTAssert(result==true)
     }
-
-    func testExample() {
-    
-        
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
