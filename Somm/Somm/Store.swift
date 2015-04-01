3b2d2786ba41ff1e8396074ba34b6d65b24ade81
Connor Knabe <ConnorKnabe@gmail.com>
2015-04-01 12:33:09 -0500

    Adding in save functionality

--- Somm/Somm/Store.swift
+++ Somm/Somm/Store.swift
@@ -28,14 +28,14 @@ class Store: NSObject {
             insertIntoManagedObjectContext:managedContext)
 
         //Save info into model
-//        visit.setValue(aVisit.arrivalDate, forKey: "arrivalDate")
-//        visit.setValue(aVisit.departureDate, forKey: "departureDate")
+       visit.setValue(aVisit.arrivalDate, forKey: "arrivalDate")
+        visit.setValue(aVisit.departureDate, forKey: "departureDate")
 //        visit.setValue(NSDate(), forKey: "arrivalDate")
 //        visit.setValue(NSDate(), forKey: "departureDate")
-//        visit.setValue(NSDate(), forKey: "timeStamp")
-//        visit.setValue(aVisit.coordinate.latitude, forKey: "latitude")
-//        visit.setValue(aVisit.coordinate.longitude, forKey: "longitude")
-//
+        visit.setValue(NSDate(), forKey: "timeStamp")
+        visit.setValue(aVisit.coordinate.latitude, forKey: "latitude")
+        visit.setValue(aVisit.coordinate.longitude, forKey: "longitude")
+
         
         //Return false if error occurs for saving
         var error: NSError?
