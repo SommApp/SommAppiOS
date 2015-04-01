8ab201f6f2d35dc68b0457c3536e73885aec2f37
Connor Knabe <ConnorKnabe@gmail.com>
2015-04-01 14:33:47 -0500

    Appending to php file

--- POST/post.php
+++ POST/post.php
@@ -1,11 +1,8 @@
 <?php
 	header('Content-type: application/json');
 	$myFile = "test.txt";
-	$fh = fopen($myFile, 'w') or die("can't open file");
-	$stringData = "Bobby Bopper\n";
-	fwrite($fh, $stringData);
-	$stringData = "Tracy Tanner\n";
-	fwrite($fh, $stringData);
+	$fh = fopen($myFile, 'a') or die("can't open file");
+
 
 	
 	if($_POST) {
